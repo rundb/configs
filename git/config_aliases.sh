34341a3c0647c4e8025bdc8fd520b3b49cc3181c
Roman Turkin <roma.turkin@gmail.com>
2022-02-22 07:28:42 +0100

    added git aliases

---
+++ git/config_aliases.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Aliases for the commands that I use most often
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.c commit
+git config --global alias.a add
+git config --global alias.st status
+git config --global alias.p push
+git config --global alias.pl pull
+git config --global alias.cl clean
+
